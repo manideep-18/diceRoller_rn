438c4dd979da3cc7fd9f5a231a0443a72ef5e47d
manideep-18 <r8manideep@gmail.com>
2020-12-06 16:37:04 +0530

    dice roller implemented with images

--- App.js
+++ App.js
@@ -1,114 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react'
+import {Text,View,Image,TouchableOpacity,StyleSheet} from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import DiceOne from './assets/dice1.png'
+import DiceTwo from './assets/dice2.png'
+import DiceThree from './assets/dice3.png'
+import DiceFour from './assets/dice4.png'
+import DiceFive from './assets/dice5.png'
+import DiceSix from './assets/dice6.png'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
+const App=()=>{
+  const [uri,setUri]=useState(DiceOne)
+
+ const  handlePlayGameButton=()=>{
+   let randomNumber=Math.floor(Math.random()*6)+1
+
+   switch(randomNumber){
+     case 1:
+       setUri(DiceOne);
+       break;
+    case 2:
+       setUri(DiceTwo);
+       break;
+    case 3:
+        setUri(DiceThree);
+        break;
+    case 4:
+          setUri(DiceFour);
+          break;
+    case 5:
+       setUri(DiceFive);
+       break;
+    case 6:
+       setUri(DiceSix);
+       break;
+      
+       default:
+         setUri(DiceOne);
+         break;
+
+
+   }
+ }
+
+  return(
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+    <View style={styles.container}>
+      <Image style={styles.image} source={uri}/>
+      <TouchableOpacity onPress={handlePlayGameButton}>
+        <Text style={styles.gamePlayButton}>Play Game</Text>
+      </TouchableOpacity>
+    </View>
     </>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+export default App
 
-export default App;
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#222831",
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  image:{
+    width:200,
+    height:200
+  },
+  gamePlayButton:{
+    fontSize:20,
+    marginTop:30,
+    color:"#F2A365",
+    borderColor:"#30475E",
+    borderRadius:5,
+    borderWidth:3,
+    paddingHorizontal:40,
+    paddingVertical:10,
+    fontWeight:"bold"
+  }
+})
